486511dacc6a91966a4e539f29627eb8966e6de0
Nate Wang <supnate@gmail.com>
2021-09-07 17:26:55 +0800

    Add api docs.

--- src/index.tsx
+++ src/index.tsx
@@ -40,20 +40,38 @@ interface NiceModalCallbacks {
   };
 }
 
+/**
+ * The handler to manage a modal returned by {@link useModal | useModal} hook.
+ */
 export interface NiceModalHandler extends NiceModalState {
   /**
-   * Whether a modal is visible, it's controlled by show/hide method.
+   * Whether a modal is visible, it's controlled by {@link NiceModalHandler.show | show}/{@link NiceModalHandler.hide | hide} method.
    */
   visible: boolean;
+  /**
+   * If you don't want to remove the modal from the tree after hide when using helpers, set it to true.
+   */
   keepMounted: boolean;
   /**
-   * Show the modal, it will change `visible` state to true.
+   * Show the modal, it will change {@link NiceModalHandler.visible | visible} state to true.
    * @param args - an object passed to modal component as props.
    */
   show: (args?: Record<string, unknown>) => Promise<unknown>;
+  /**
+   * Hide the modal, it will change {@link NiceModalHandler.visible | visible} state to false.
+   */
   hide: () => void;
+  /**
+   * Resolve the promise returned by {@link NiceModalHandler.show | show} method.
+   */
   resolve: (args?: unknown) => void;
+  /**
+   * Reject the promise returned by {@link NiceModalHandler.show | show} method.
+   */
   reject: (args?: unknown) => void;
+  /**
+   * Remove the modal component from React component tree. It improves performance compared to just making a modal invisible.
+   */
   remove: () => void;
 }
 
