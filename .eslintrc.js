d34731a24aeff23545d4fe43131aa2df336d13af
Govind <gnsharma0810@gmail.com>
2022-04-27 13:39:43 +0530

    fix: support providing props to NiceModal.create as interface without index signature

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,4 +15,26 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
   },
+	overrides: [
+    {
+      // For react component files, we want to allow types of the form `type Props = {}`
+      // as Props is guaranteed to be an object by React. Without overriding this rule,
+      // we would need to extend the Props generic from Record<string, unknown>
+      // which causes issues when consumer props are defined as interface without an index signature.
+      // Refer to react part of the comment for more details.
+      // https://github.com/typescript-eslint/typescript-eslint/issues/2063#issuecomment-675156492
+      excludedFiles: "src/*.tsx",
+      rules: {
+        "@typescript-eslint/ban-types": [
+          "error",
+          {
+            extendDefaults: true,
+            types: {
+              "{}": false,
+            },
+          },
+        ],
+      },
+    },
+  ],
 };
